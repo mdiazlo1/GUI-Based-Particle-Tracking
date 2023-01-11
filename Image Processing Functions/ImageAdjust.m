498f6d353fb0de135d660eace1d4a56117bfd9d9
Miguel X. Diaz-Lopez <mdialo1@jhu.edu>
2023-01-11 11:38:30 -0500

    Fixing name of ImageProcessing Function

--- Image Processing Functions/ImageAdjust.m
+++ Image Processing Functions/ImageAdjust.m
@@ -1,7 +1,7 @@
 function AdjustedImg = ImageAdjust(img,adjustlow,adjusthigh,gamma)
 classtype = class(img(:,:,1));
 AdjustedImg = zeros(size(img),classtype);
-for i = 1:size(img,3)
+parfor i = 1:size(img,3)
     AdjustedImg(:,:,i) = imadjust(img(:,:,i),[adjustlow adjusthigh],[],gamma);
 end
 
--- Image Processing Functions/ImageSharpening.m
+++ Image Processing Functions/ImageSharpening.m
@@ -1,7 +1,7 @@
 function SharpenedImg = ImageSharpening(img,Radius,Amount,Threshold)
 classtype = class(img(:,:,1));
 SharpenedImg = zeros(size(img),classtype);
-for i = 1:size(img,3)
+parfor i = 1:size(img,3)
     SharpenedImg(:,:,i) = imsharpen(img(:,:,i),"Radius",Radius,"Amount",Amount,"Threshold",Threshold);
 end
 
