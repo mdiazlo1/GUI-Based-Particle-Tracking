813b6ed3556341a1ac18d487a38dd1395ba66789
Miguel X. Diaz-Lopez <mdialo1@jhu.edu>
2023-01-11 17:52:19 -0500

    Updating to add background subtraction

--- ImageProcessingAndTrackingCalling.m
+++ ImageProcessingAndTrackingCalling.m
@@ -1,5 +1,5 @@
-function [vtracks,tracks] = ImageProcessingAndTrackingCalling(Settings,inputnames,SaveDirec,frame_list)
-frame_list = 0;
+function [vtracks,tracks] = ImageProcessingAndTrackingCalling(settings,inputnames,SaveDirec,frame_list)
+% frame_list = 0;
 
 addpath(genpath('.'))
 % load('.\Testing\Settings.mat')
@@ -26,9 +26,11 @@ end
 parfor i = 1:numel(frame_list)
     imwrite(img(:,:,i),[SaveDirec filesep 'data_' sprintf('%04d',frame_list(i)) '.tif'])
 end
-settings.area_lim = flip(settings.area_lim);
+% settings.area_lim = flip(settings.area_lim);
 %Tracking
 [vtracks,~,~,~,tracks] = PredictiveTracker([SaveDirec '\*.tif'],settings.BinaryThresh,settings.max_disp,[],settings.area_lim);
+
+
 
 end
 
