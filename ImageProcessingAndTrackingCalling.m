2e41e1b9f79151334ed6b551bd20814b0a9ef42b
Miguel X. Diaz-Lopez <mdialo1@jhu.edu>
2023-01-11 11:39:27 -0500

    Adding Image Processing Function

---
+++ ImageProcessingAndTrackingCalling.m
@@ -0,0 +1,36 @@
+function [vtracks,tracks] = ImageProcessingAndTrackingCalling(Settings,inputnames,SaveDirec,frame_list)
+frame_list = 0;
+
+addpath(genpath('.'))
+% load('.\Testing\Settings.mat')
+% inputnames = "E:\PIV Data\Processed Data\2022_06_23\T2\HistMatchImages\R1\*.tiff";
+% SaveDirec = 'E:\PIV Data\Processed Data\2022_06_23\T2\PTVGasPhase\R1';
+
+settings.gamma = settings.Gamme;
+
+filelist = dir(inputnames);
+
+ImageNames = {filelist.name}; ImageFolder = filelist(1).folder; clearvars filelist
+img = LoadingImages(ImageFolder,ImageNames,0);
+
+%Image Contrast Adjustment
+img = ImageAdjust(img,settings.adjustlow,settings.adjusthigh,settings.gamma);
+
+%Image Sharpening
+img = ImageSharpening(img,settings.SharpenRadius,settings.SharpenAmount,settings.SharpenThreshold);
+
+%Saving Images
+if frame_list == 0
+    frame_list = 0:numel(ImageNames)-1;
+end
+parfor i = 1:numel(frame_list)
+    imwrite(img(:,:,i),[SaveDirec filesep 'data_' sprintf('%04d',frame_list(i)) '.tif'])
+end
+settings.area_lim = flip(settings.area_lim);
+%Tracking
+[vtracks,~,~,~,tracks] = PredictiveTracker([SaveDirec '\*.tif'],settings.BinaryThresh,settings.max_disp,[],settings.area_lim);
+
+end
+
+
+
