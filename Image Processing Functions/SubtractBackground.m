c5e802a39f229d5ffecd658424a1a0a6f4ce29f4
Miguel X. Diaz-Lopez <mdialo1@jhu.edu>
2023-01-12 14:42:50 -0500

    making changes to background processing

--- Image Processing Functions/SubtractBackground.m
+++ Image Processing Functions/SubtractBackground.m
@@ -1,31 +1,33 @@
 % function SubtractBackground(img,frameskip,rollingwindow)
 classtype = class(img(:,:,1));
 rollingwindow = 0;
+frameskip = 1;
 if rollingwindow
     for i = 1:rollingwindow/2:size(img,3)
         if i<rollingwindow/2+1
             %Forward averaging
-            img_bg = mean(double(img(:,:,1:i+rollingwindow/2)),3)
+            img_bg = mean(double(img(:,:,1:frameskip:i+rollingwindow/2)),3)
 
             img(:,:,i:i+rollingwindow/2) = double(img(:,:,i:i+rollingwindow/2))-img_bg
         elseif i>size(img,3)-rollingwindow
             %Backward averaging
-            img_bg = mean(double(img(:,:,i-rollingwindow/2:size(img,3))),3);
+            img_bg = mean(double(img(:,:,i-rollingwindow/2:frameskip:size(img,3))),3);
 
             img(:,:,i:size(img,3)) = double(img(:,:,i:size(img,3)))-A_bg;
         else
             %Central averaging
             img_bg = mean(img(:,:,i-rollingwindow/2:i+rollingwindow/2),3);
 
-            img = double(img(:,:,i-rollingwindow/2:i+rollingwindow/2),3)
+            img = double(img(:,:,i-rollingwindow/2:i+rollingwindow/2)-A_bg,3)
         end
     end
 else
-    csum = sum(double(img),3);
+    csum = sum(double(img(:,:,1:frameskip:size(img,3))),3);
     img_bg = csum./numel(img);
-    img = cast(double(img)-img_bg,classtype);
+%     img_bg = mean(double(img(:,:,1:frameskip:size(img,3))),3);
+    img = double(img)-img_bg;
 end
-
-% img = cast(double(img)-img_bg,classtype);
+clearvars img_bg
+img = cast(img,classtype);
 
 % end
